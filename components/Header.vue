26709a633e2900f4e8f9c0c815d42e4a6e6819ce
Hartaithan <hartaithan@gmail.com>
2023-09-09 19:23:11 +0900

    add layout and header

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <nav class="h-12 bg-neutral-800 flex justify-center items-center gap-6">
+      <NuxtLink to="/" active-class="text-green-600 font-bold">
+        Characters
+      </NuxtLink>
+      <NuxtLink to="/locations">
+        Locations
+      </NuxtLink>
+      <NuxtLink to="/episodes">
+        Episodes
+      </NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <Header />
+  <main class="grow max-w-screen-xl py-4">
+    <slot />
+  </main>
+</template>
