9012f6d89c63caf2beb763affbf7ce9753065339
Hartaithan <hartaithan@gmail.com>
2023-09-09 11:19:25 +0900

    project cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Ramutable App",
+    },
+  },
+  css: ["@/assets/css/main.css"],
 });
