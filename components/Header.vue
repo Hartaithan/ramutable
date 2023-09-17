bfe70bd8d34275aa9cf5a0f0bbc163fd3763578e
Hartaithan <hartaithan@gmail.com>
2023-09-17 13:13:04 +0900

    server side navigation

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,25 @@
+<script>
+export default {
+  methods: {
+    isRouteActive(id) {
+      return this.$route.path === id;
+    },
+  },
+};
+</script>
+
 <template>
   <header>
     <nav class="h-12 bg-neutral-800 flex justify-center items-center gap-6">
-      <NuxtLink to="/" active-class="text-green-600 font-bold">
-        Characters
-      </NuxtLink>
-      <NuxtLink to="/locations" active-class="text-green-600 font-bold">
-        Locations
-      </NuxtLink>
-      <NuxtLink to="/episodes" active-class="text-green-600 font-bold">
-        Episodes
-      </NuxtLink>
+      <a :class="{ active: isRouteActive('/') }" href="/">Characters</a>
+      <a :class="{ active: isRouteActive('/locations') }" href="/locations">Locations</a>
+      <a :class="{ active: isRouteActive('/episodes') }" href="/episodes">Episodes</a>
     </nav>
   </header>
 </template>
+
+<style scoped>
+.active {
+  @apply text-green-600 font-bold
+}
+</style>
